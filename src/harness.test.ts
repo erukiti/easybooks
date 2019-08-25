3ba16a854a72e12f2fe00ff66115a7fc6b9f572d
SASAKI Shunsuke <erukiti@gmail.com>
2019-08-25 18:51:37 +0900

    テストハーネスを、YAML.safeload で書き直して、見やすく・書きやすくした

--- src/harness.test.ts
+++ src/harness.test.ts
@@ -1,10 +1,12 @@
-import { buildBook } from './build-book'
-
 import fs from 'fs'
 import os from 'os'
 import path from 'path'
 import { promisify } from 'util'
 import childProcess from 'child_process'
+
+import yaml from 'js-yaml'
+
+import { buildBook } from './build-book'
 
 const mkdtemp = promisify(fs.mkdtemp)
 const writeFile = promisify(fs.writeFile)
@@ -51,21 +53,23 @@ describe('buildBook test harness', () => {
     ]
     await Promise.all(preTasks)
     await buildBook(path.join(tmpDir, 'test.json'))
+
     expect(
-      await readFile(path.join(reviewDir, 'catalog.yml'), {
-        encoding: 'utf-8',
-      }),
-    ).toEqual('CHAPS:\n  - hoge.re\n')
+      yaml.safeLoad(
+        await readFile(path.join(reviewDir, 'catalog.yml'), {
+          encoding: 'utf-8',
+        }),
+      ),
+    ).toEqual({ CHAPS: ['hoge.re'] })
+
     expect(
-      await readFile(path.join(reviewDir, 'config.yml'), {
-        encoding: 'utf-8',
-      }),
-    ).toEqual(`aut:
-  - なまえ
-texstyle:
-  - reviewmacro
-review_version: 3
-`)
+      yaml.safeLoad(
+        await readFile(path.join(reviewDir, 'config.yml'), {
+          encoding: 'utf-8',
+        }),
+      ),
+    ).toEqual({ aut: ['なまえ'], texstyle: ['reviewmacro'], review_version: 3 })
+
     const st = await stat(path.join(reviewDir, 'example.pdf'))
     expect(st.isFile()).toBeTruthy()
     expect(st.size).toBeGreaterThan(1000)
